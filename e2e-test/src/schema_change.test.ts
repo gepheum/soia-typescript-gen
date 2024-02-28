7ddf1639b0549b27ef03bf055b0ceda30a27314e
Tyler Fibonacci <gepheum@gmail.com>
2024-02-27 20:02:34 -0800

    handle unrecognized enum fields

--- e2e-test/src/schema_change.test.ts
+++ e2e-test/src/schema_change.test.ts
@@ -3,53 +3,96 @@ import {
   EnumAfter,
   FooAfter,
   FooBefore,
+  RemovalAfter,
+  RemovalBefore,
 } from "../soiagen/schema_change.soia.js";
 import { expect } from "buckwheat";
 import { describe, it } from "mocha";
 
-describe("unrecognized fields", () => {
-  const foo = FooAfter.create({
-    bars: [
-      BarAfter.create({
-        x: 2.5,
-        s: "S0",
-      }),
-      BarAfter.create({
-        x: 2.0,
-        s: "S1",
-      }),
-      BarAfter.DEFAULT,
-      BarAfter.create({
-        x: 3.0,
-      }),
-    ],
-    n: 100,
-    enums: [
-      EnumAfter.UNKNOWN,
-      EnumAfter.A,
-      EnumAfter.B,
-      EnumAfter.create("c", "CcC"),
-    ],
-    bit: true,
+describe("schema change", () => {
+  describe("unrecognized fields", () => {
+    const foo = FooAfter.create({
+      bars: [
+        BarAfter.create({
+          x: 2.5,
+          s: "S0",
+        }),
+        BarAfter.create({
+          x: 2.0,
+          s: "S1",
+        }),
+        BarAfter.DEFAULT,
+        BarAfter.create({
+          x: 3.0,
+        }),
+      ],
+      n: 100,
+      enums: [
+        EnumAfter.UNKNOWN,
+        EnumAfter.A,
+        EnumAfter.B,
+        EnumAfter.create("c", "CcC"),
+      ],
+      bit: true,
+    });
+
+    it("are kept in JSON format", () => {
+      const fooBefore = FooBefore.SERIALIZER.fromJson(
+        FooAfter.SERIALIZER.toJson(foo),
+      );
+      const reserialized = FooAfter.SERIALIZER.fromJson(
+        FooBefore.SERIALIZER.toJson(fooBefore),
+      );
+      expect(reserialized).toMatch(foo);
+    });
+
+    it("are kept in binary format", () => {
+      const fooBefore = FooBefore.SERIALIZER.fromBytes(
+        FooAfter.SERIALIZER.toBytes(foo).toBuffer(),
+      );
+      const reserialized = FooAfter.SERIALIZER.fromBytes(
+        FooBefore.SERIALIZER.toBytes(fooBefore).toBuffer(),
+      );
+      expect(reserialized).toMatch(foo);
+    });
   });
 
-  it("are kept in JSON format", () => {
-    const fooBefore = FooBefore.SERIALIZER.fromJson(
-      FooAfter.SERIALIZER.toJson(foo),
-    );
-    const reserialized = FooAfter.SERIALIZER.fromJson(
-      FooBefore.SERIALIZER.toJson(fooBefore),
-    );
-    expect(reserialized).toMatch(foo);
-  });
+  describe("removed fields", () => {
+    const before = RemovalBefore.create({
+      a: "AA",
+      b: "BB",
+      es: [
+        RemovalBefore.E.C,
+        RemovalBefore.E.D,
+        RemovalBefore.E.create("e", "EE"),
+      ],
+    });
 
-  it("are kept in binary format", () => {
-    const fooBefore = FooBefore.SERIALIZER.fromBytes(
-      FooAfter.SERIALIZER.toBytes(foo).toBuffer(),
-    );
-    const reserialized = FooAfter.SERIALIZER.fromBytes(
-      FooBefore.SERIALIZER.toBytes(fooBefore).toBuffer(),
-    );
-    expect(reserialized).toMatch(foo);
+    const expected = RemovalBefore.create({
+      b: "BB",
+      es: [RemovalBefore.E.C, RemovalBefore.E.UNKNOWN, RemovalBefore.E.UNKNOWN],
+    });
+
+    it("are removed when deserializing from JSON", () => {
+      const reserialized = RemovalBefore.SERIALIZER.fromJson(
+        RemovalAfter.SERIALIZER.toJson(
+          RemovalAfter.SERIALIZER.fromJson(
+            RemovalBefore.SERIALIZER.toJson(before),
+          ),
+        ),
+      );
+      expect(reserialized).toMatch(expected);
+    });
+
+    it("are removed when deserializing from bytes", () => {
+      const reserialized = RemovalBefore.SERIALIZER.fromBytes(
+        RemovalAfter.SERIALIZER.toBytes(
+          RemovalAfter.SERIALIZER.fromBytes(
+            RemovalBefore.SERIALIZER.toBytes(before).toBuffer(),
+          ),
+        ).toBuffer(),
+      );
+      expect(reserialized).toMatch(expected);
+    });
   });
 });
